ecfdf27860767deda8d14025f9a26191000259d0
cintia_sestelo <cintia.sestelo@sieve.com.br>
2018-11-29 15:51:07 -0200

    0.4.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='requery',
-    version='0.4.0.0',
+    version='0.4.1.0',
     url='http://github.com/ebertti/requery/',
     author='Ezequiel Bertti',
     author_email='ebertti@gmail.com',
